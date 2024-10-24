f949736613704de360573d2521414201ebcba27e
rookie <liuxili@bestseller.com.cn>
2024-10-24 11:51:52 +0800

    init

---
+++ src/main/java/com/rookie/bigdata/RookieStudyVueApplication.java
@@ -0,0 +1,13 @@
+package com.rookie.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RookieStudyVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RookieStudyVueApplication.class, args);
+    }
+
+}
